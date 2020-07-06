2a85cb845e047dc33d25c77a7eb6848203f840e7
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-06 19:10:01 -0400

    added post route to add items to database

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express")
 const app = express();
 const path = require("path")
 const PORT = process.env.PORT || 3000;
+const database = require("./db/db")
 
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }));
@@ -17,9 +18,19 @@ app.get("/notes", (req, res)=>{
     res.sendFile(path.join(__dirname, "./public/notes.html"))
 })
 
+app.get("/api/notes", (req, res)=>{
+    res.json(database)
+})
 app.get("*", (req, res)=>{
     res.sendFile(path.join(__dirname, "./public/index.html"))
 })
+
+app.post("/api/notes", function(req, res) {
+    let newNote = req.body;
+    database.push(newNote);
+    res.json(newNote);
+  });
+
 
 
 app.listen(PORT, (req, res) =>{
