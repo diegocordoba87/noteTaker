636dc74e8a57d800ae7ffbc3c3122dda773a06e1
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-06 18:22:47 -0400

    added express.static

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 //rrquire express
 const express = require("express")
-const path = require("path")
 const app = express();
+const path = require("path")
 const PORT = process.env.PORT || 3000;
-app.use(express.static("public"));
 
+app.use(express.static("public"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/notes", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./develop/public/notes.html"))
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./public/index.html"))
 })
 
-app.get("/*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./develop/public/index.html"))
+app.get("/notes", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./public/index.html"))
 })
 
 
