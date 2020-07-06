5be72f2d17ce6e5cbec0aafd08ea46875e0511b4
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-06 17:34:50 -0400

    added get/ view routes

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 //rrquire express
 const express = require("express")
-
+const path = require("path")
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res)=>{
-    res.send("hello world")
+app.get("/notes", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./develop/public/notes.html"))
+})
+
+app.get("/*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "./develop/public/index.html"))
 })
 
 
