4b916e1bc9b3a124a4ba1ec09aca85614ea0d0cc
Diego Cordoba <diegocordoba87@hotmail.com>
2020-07-07 23:44:36 -0400

    changed memory to be persisten while server runs and not writing to the .jsno file

--- server.js
+++ server.js
@@ -1,53 +1,20 @@
-//rrquire express
-const express = require("express")
+const express = require("express");
 const app = express();
-const path = require("path")
+const path = require("path");
 const PORT = process.env.PORT || 3000;
-const database = require("./db/db.json");
-const fs = require("fs");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-app.use(express.static(path.join(__dirname, "public")))
+
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/api",apiRoutes);
+app.use("/",htmlRoutes);
 
 
-app.get("/", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-})
 
-app.get("/notes", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
-})
 
-app.get("/api/notes", (req, res)=>{
-    res.json(database)
-})
-app.get("*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "./public/index.html"))
-})
-
-app.post("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", (err, data) => {
-    const notes = JSON.parse(data);
-    
-    
-    let newNote = {
-        id: notes.length +1,
-        title: req.body.title,
-        text: req.body.text
-    }
-    
-    notes.push(newNote)
-
-    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
-        if (!err) console.log("it worked");       
-      })      
-    }) 
-    res.json(database)   
-})
-
-app.listen(PORT, (req, res) =>{
-
-console.log(`currently listening to http://localhost:${PORT}`)
-
-})
+app.listen(PORT, (req, res) => {
+  console.log(`currently listening to http://localhost:${PORT}`);
+});
